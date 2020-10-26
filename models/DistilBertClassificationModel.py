896c39f33055f151325f5a60bd42d258ce034581
AbhiSivaprasad <abhi@live.com>
2020-10-26 04:57:46 +0000

    simple classification model built on huggingface DistilBert

---
+++ models/DistilBertClassificationModel.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from transformers import DistilBertModel
+
+# Simple dropout + dense layer on top of DistillBERT
+class DistilBertClassificationModel(torch.nn.Module):
+    def __init__(self, output_size):
+        super(DistillBERTClass, self).__init__()
+        self.model = DistilBertModel.from_pretrained("distilbert-base-uncased")
+        self.pre_classifier = nn.Linear(768, 768)
+        self.dropout = nn.Dropout(0.3)
+        self.classifier = nn.Linear(768, output_size)
+
+    def forward(self, input_ids, attention_mask):
+        output_1 = self.model(input_ids=input_ids, attention_mask=attention_mask)
+        hidden_state = output_1[0]
+        pooler = hidden_state[:, 0]
+        pooler = self.pre_classifier(pooler)
+        pooler = torch.nn.ReLU()(pooler)
+        pooler = self.dropout(pooler)
+        output = self.classifier(pooler)
+        return output
