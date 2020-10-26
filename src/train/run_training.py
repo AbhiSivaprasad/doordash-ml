4a39cc5d7d28447fd5050bb04b7ae2dc84956a73
AbhiSivaprasad <abhi@live.com>
2020-10-26 07:56:01 +0000

    working on train controller

--- src/train/run_training.py
+++ src/train/run_training.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from ./utils import set_seed
+from .utils import set_seed
+
 
 def run_training(args: TrainArgs):
     # set seed for reproducibility
@@ -11,4 +12,15 @@ def run_training(args: TrainArgs):
     # create data splits
     train_data, valid_data, test_data = split_data(data)
     
+    # run training
     train(args, train_data, valid_data, test_data, logger)
+
+    # Split train/test
+    rand = np.random.rand(len(data))
+    train_data = df[rand < args.train_size]
+    valid_data = df[rand > args.train_size and rand < args.train_size + args.valid_size]
+    test_data = df[rand > args.train_size + args.valid_size]
+
+    # pass in targets to dataset
+    training_set = L1Dataset(train_dataset, tokenizer, MAX_LEN)
+    testing_set = L1Dataset(test_dataset, tokenizer, MAX_LEN)
