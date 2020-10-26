ce762f5a02d52b865b7e1f88d5d255102781ebe2
AbhiSivaprasad <abhi@live.com>
2020-10-26 21:03:53 +0000

    moved bert dataset

---
+++ src/data/bert.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+class BertDataset(Dataset):
+    def __init__(self, data: pd.DataFrame, tokenizer, max_len: int):
+        """
+        :param data: Pandas Dataframe containing Dataset. 
+                     Column "target" contains int target class. Column "name" contains str item name
+        """
+        self.len = len(data)
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        # TODO: batch tokenize 
+        inputs = self.tokenizer(
+            str(self.data.name[index]),
+            add_special_tokens=True,
+            max_length=self.max_len,
+            padding='max_length',
+            truncation=True # TODO: measure performance of truncated samples
+        )
+        
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'targets': torch.tensor(self.data.target[index], dtype=torch.long)
+        } 
+
+    def __len__(self):
+        return self.len
+
+    @property
+    def targets():
+        return self.data["target"]
