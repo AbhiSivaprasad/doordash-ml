d1b08e5a9967c98d9112ceda4db892b1ebc88f8d
AbhiSivaprasad <abhi@live.com>
2020-10-28 05:20:50 +0000

    updated batch prediction and evaluation

--- src/train/batch_predict.py
+++ src/train/batch_predict.py
@@ -15,28 +15,21 @@ def batch_predict(*args, strategy='greedy', **kwargs):
 
 def batch_predict_greedy(l1_model: nn.Module,
                          l2_models_dict: Dict[int, nn.Module], 
-                         data_loader: DataLoader) -> List[int]:
+                         data_loader: DataLoader) -> List[Tuple[int, int]]:
     """Compute batch predictions greedily by maximizing L1, L2 classifiers separately
 
     :param l2_models_dict: key = int L1 class id, value = corresponding L2 model
     """
     # compute L1 predictions
     l1_predictions = np.array(predict(l1_model, data_loader))
+    l2_predictions = np.zeros(len(l1_predictions))
 
     # compute all L2 predictions
-    all_predictions = np.full((len(l1_predictions), len(l2_models_dict)), -1)
     for l1_class_id, model in l2_models_dict.items():
-        all_predictions[:, l1_class_id] = predict(model, data_loader)
-
-    # make sure all predictions are filled in 
-    if -1 in all_predictions:
-        raise Exception("Some predictions missing")
+        l2_predictions[l1_predictions == l1_class_id] = predict(model, data_loader)
     
-    # gather predictions
-    final_preds = np.take_along_axis(all_predictions, l1_predictions[:, np.newaxis], axis=1)
-
-    # return final predictions
-    return list(final_preds)
+    # return as a list of tuples
+    return list(zip(l1_predictions, l2_predictions))
 
 
 def batch_predict_complete_search():
--- src/train/evaluate.py
+++ src/train/evaluate.py
@@ -13,3 +13,25 @@ def evaluate_predictions(preds: List[int],
 
     # Currently measure only acc
     return (preds == targets).sum() / len(targets)
+
+
+def evaluate_batch_predictions(preds: List[Tuple[int]], targets: List[Tuple[int]], num_classes: int):
+    """Compute accuracy given predictions and targets
+    
+    :param preds: List of Tuples with predicted class ids. Format: [(L1, L2), (L1, L2), ...]
+    """
+    preds = np.array(preds)
+    targets = np.array(targets)
+
+    scores = preds == targets
+
+    # L1 accuracy and Overall accuracy
+    l1_acc = scores[:, 0].mean()
+    total_acc = scores.all(axis=1).mean()
+
+    # L2 accuracy per class
+    l2_accs = {}  # key = class id, value = acc
+    for class_id in range(num_classes):
+        l2_accs[class_id] = scores[targets == class_id].all(axis=1).mean()
+
+    return total_acc, l1_acc, l2_accs
