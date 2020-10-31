32aec2be2d760f99b02a6f976d1d8866a8cd9fee
AbhiSivaprasad <abhi@live.com>
2020-10-31 04:29:24 +0000

    updated prediction controller to use with confidence scores

--- src/train/run_predictions.py
+++ src/train/run_predictions.py
@@ -1,13 +1,15 @@
+import numpy as np
 import pandas as pd
 
-from os import listdir
+from os import listdir, makedirs
 from os.path import join, isdir
 from torch.utils.data import DataLoader
+from pathlib import Path
 
 from .utils import load_best_model, DefaultLogger, load_checkpoint
 from .batch_predict import batch_predict
 from .predict import predict
-from .evaluate import evaluate_batch_predictions, evaluate_predictions
+from .evaluate import evaluate_batch_predictions, evaluate_predictions, evaluate_lr_precision
 from ..args import PredictArgs
 from ..data.bert import BertDataset
 from ..data.taxonomy import Taxonomy
@@ -20,6 +22,7 @@ def run_predictions(args: PredictArgs):
     Construct hierarchy of models and run predictions
     """
     logger = DefaultLogger()
+    Path(args.save_dir).mkdir(parents=True, exist_ok=True)  # create logging dir
 
     # get raw test data
     test_data = pd.read_csv(args.test_path)
@@ -43,10 +46,17 @@ def run_predictions(args: PredictArgs):
     test_dataloader = DataLoader(test_data, batch_size=args.batch_size)
 
     # compute predictions and targets
-    l1_targets = list(test_data.data["L1_target"])
-    l2_targets = list(test_data.data["L2_target"])
-    targets = list(zip(l1_targets, l2_targets))
-    preds = batch_predict(l1_model, l2_models_dict, test_dataloader, args.device, strategy=args.strategy)
+    l1_targets = test_data.data["L1_target"].values
+    l2_targets = test_data.data["L2_target"].values
+    targets = np.concatenate((l1_targets[:, np.newaxis], l2_targets[:, np.newaxis]), axis=1)
+
+    preds, l1_confidence_scores, l2_confidence_scores = batch_predict(
+        l1_model, l2_models_dict, test_dataloader, args.device, strategy=args.strategy)
+    
+    # evaluate precision, recall with confidence scores
+    confidence_ordering = l2_confidence_scores.argsort()[::-1]  
+    scores = (preds == targets).all(axis=1)
+    precisions = evaluate_lr_precision(scores)
 
     # compute prediction accuracy
     overall_acc, l1_overall_acc, l1_class_accs = \
@@ -60,3 +70,5 @@ def run_predictions(args: PredictArgs):
     for class_id, l1_class_acc in l1_class_accs.items():
         class_name = taxonomy.class_id_to_category(class_id)
         logger.debug(f"L1 Category {class_name}:", l1_class_acc)
+
+    pd.DataFrame(precisions).to_csv(join(args.save_dir, "lr_precisions.csv"), header=False, index=False)
