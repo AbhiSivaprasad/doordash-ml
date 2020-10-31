3f9aac2c2f64eaa0f6f4848aa5b57e016dbba5e0
AbhiSivaprasad <abhi@live.com>
2020-10-31 03:53:29 +0000

    updated batch prediction with confidence scores

--- src/train/batch_predict.py
+++ src/train/batch_predict.py
@@ -13,7 +13,8 @@ def batch_predict(l1_model: nn.Module,
                   l2_models_dict: Dict[int, nn.Module], 
                   data_loader: DataLoader,
                   device: torch.device, 
-                  strategy='greedy', 
+                  strategy: str = 'greedy', 
+                  return_probs: bool = False,
                   **kwargs):
     """Dispatch appropriate prediction function based on strategy"""
     if strategy == 'greedy':
@@ -27,24 +28,40 @@ def batch_predict(l1_model: nn.Module,
 def batch_predict_greedy(l1_model: nn.Module,
                          l2_models_dict: Dict[int, nn.Module], 
                          data_loader: DataLoader,
-                         device: torch.device) -> List[Tuple[int]]:
+                         device: torch.device) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Compute batch predictions greedily by maximizing L1, L2 classifiers separately
 
     :param l2_models_dict: key = int L1 class id, value = corresponding L2 model
     """
     # compute L1 predictions
     l1_model.to(device)
-    l1_preds = np.array(predict(l1_model, data_loader, device))
+    l1_preds, l1_probs = predict(l1_model, data_loader, device, return_probs=True)
+
+    # track L2 predictions and probabilities
     l2_preds = np.zeros(len(l1_preds), dtype=int)
+    l2_probs = np.zeros(len(l1_preds))
 
     # compute all L2 predictions
     for l1_class_id, model in tqdm(l2_models_dict.items()):
         model.to(device)
+
+        l2_class_preds, l2_class_probs = predict(model, data_loader, device, return_probs=True)
+
+        # select only data predicted as current category
         mask = (l1_preds == l1_class_id)
-        l2_preds[mask] = np.array(predict(model, data_loader, device))[mask]
+
+        # select the l2_preds, and l2_probs for the current category 
+        l2_preds[mask] = l2_class_preds[mask]
+        l2_probs[mask] = l2_class_probs[mask]  
+
+        # move model back to save gpu memory
+        model.to(torch.device('cpu'))
     
-    # return as a list of tuples
-    return list(zip(l1_preds, l2_preds))
+    # join predictions. (batch, 2)
+    preds = np.concatenate((l1_preds, l2_preds), axis=1)
+
+    # preds, L1 confidence, L1 & L2 confidence
+    return preds, l1_probs, l1_probs * l2_probs
 
 
 def batch_predict_complete_search(l1_model: nn.Module,
