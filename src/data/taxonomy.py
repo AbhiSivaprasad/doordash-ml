c6ed727290c70dca52a515ea291e9ea141191d6b
AbhiSivaprasad <abhi@live.com>
2020-11-14 03:31:10 +0000

    made several changes to taxonomy structure

--- src/data/taxonomy.py
+++ src/data/taxonomy.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import pandas as pd
 
 from typing import List, Dict, Optional, Tuple
 from os.path import join
@@ -10,7 +11,8 @@ class TaxonomyNode:
     def __init__(self, 
                  category_id: int,
                  category_name: str, 
-                 class_id: Optional[int], 
+                 class_id: Optional[int] = None, 
+                 vendor_id: Optional[int] = None,
                  parent: 'TaxonomyNode' = None,
                  children: List['TaxonomyNode'] = None):
         """
@@ -19,10 +21,15 @@ class TaxonomyNode:
         self._category_id = category_id
         self._category_name = category_name
         self._class_id = class_id
+        self._vendor_id = vendor_id
         self._parent = parent
         self._children = children if children is not None else []
 
-    def add_child(self, category_id: int, category_name: str, class_id: Optional[int]):
+    def add_child(self, 
+                  category_id: int, 
+                  category_name: str, 
+                  class_id: Optional[int] = None, 
+                  vendor_id: Optional[int] = None):
         """
         Add child node to taxonomy node with name category_name
 
@@ -31,6 +38,7 @@ class TaxonomyNode:
         child = TaxonomyNode(category_id=category_id,
                              category_name=category_name,
                              class_id=class_id if class_id is not None else len(self.children),
+                             vendor_id=vendor_id,
                              parent=self)
 
         # add child node and return it
@@ -44,7 +52,7 @@ class TaxonomyNode:
         Class ids of children will be shifted to stay consecutive
         """
         matches = [i for i, _ in enumerate(self._children) 
-                   if c._category_id = category_id]
+                   if c._category_id == category_id]
 
         if len(matches) == 0:
             raise ValueError(f"Node with id {category_id} not found")
@@ -77,6 +85,10 @@ class TaxonomyNode:
         return self._class_id
 
     @property
+    def vendor_id(self):
+        return self._class_id
+
+    @property
     def children(self):
         return self._children
 
@@ -87,10 +99,10 @@ class TaxonomyNode:
 
 class Taxonomy:
     def __init__(self, root: TaxonomyNode = None):
-        # create generic root node if not passed
+        # create generic root node if not passed, with id 0
         self._root = (root 
                       if root is not None 
-                      else TaxonomyNode(category_name="root", class_id=None))
+                      else TaxonomyNode(category_id=0, category_name="root"))
 
     def __str__(self) -> str:
         """readable represenation of taxonomy"""
@@ -106,24 +118,35 @@ class Taxonomy:
     def iter_level(self, level: int = 1):
         return self._iter_level(self._root, level)    
 
-    def get_max_level():
+    def get_max_level(self):
         """
         Return max level (L1, L2, etc.)
         Equivalent to the max depth - 1 (root)
         """
-        return max(self.iter(), lambda x: len(x[1]))
+        max_level = 0
+        for node, path in self.iter():
+            if max_level < len(path):
+                max_level = len(path)
 
-    def add(self, parent_category_id: id, category_id: int, category_name: str, class_id: Optional[int] = None):
+        # don't count root in depth
+        return max_level - 1
+
+    def add(self, 
+            parent_category_id: id, 
+            category_id: int, 
+            category_name: str, 
+            class_id: Optional[int] = None, 
+            vendor_id: Optional[int] = None):
         """
         Add child node to taxonomy, child's class id is assigned as index in parent's children
         """
         parent_node, _ = self.find_node_by_id(parent_category_id)
 
         if parent_node is None:
-            raise ValueError(f"No node found with category: {parent_category}")
+            raise ValueError(f"Parent node not found with category id: {parent_category_id}")
 
         # create child node and add to parent
-        child_node = parent_node.add_child(category_id, category_name, class_id)
+        child_node = parent_node.add_child(category_id, category_name, class_id, vendor_id)
         return child_node
 
     def remove(self, category_id: str) -> None:
@@ -160,62 +183,83 @@ class Taxonomy:
 
         return None, None
 
-    def read(self, dir_path: str) -> 'Taxonomy':
-        """Read the human readable representation into native data structure"""
+    def from_csv(self, filepath: str) -> 'Taxonomy':
+        """Read csv into native data structure"""
         # read taxonomy from dir
+        return self.from_df(pd.from_csv(filepath))
+
+    def from_df(self, df: pd.DataFrame) -> 'Taxonomy':
+        """Read df into native data structure"""
         taxonomy = Taxonomy()
-        with open(join(dir_path, "taxonomy.csv"), 'r') as f:
-            reader = csv.DictReader(f)
 
-            # parse each row
-            for row in reader:
-                # compute depth of row
-                depth = 1
-                while f"L{depth}" in row and row[f"L{depth}"] != "":
-                    depth += 1
-                depth -= 1
+        # sort taxonomy entries by number of NaNs so ordering: L1, L2, ..., Lx categories
+        # ensures that children are processed after parent
+        df = df.iloc[df.isnull().sum(1).sort_values(ascending=False).index]
+        
+        for _, row in df.iterrows():
+            depth = 1
+            while f"L{depth}" in row and not pd.isnull(row[f"L{depth}"]):
+                depth += 1
+            depth -= 1
 
-                # extract node information 
-                parent_id = int(row[f"L{depth - 1} Category ID"])
-                category_id = int(row[f"L{depth} Category ID"])
-                category_name = row[f"L{depth}"]
-                class_id = int(row["Class ID"]) if "Class ID" in row else None
+            # extract node information 
+            parent_id = (int(row[f"L{depth - 1} Category ID"]) 
+                         if depth > 1 
+                         else taxonomy._root.category_id)
+            category_id = int(row[f"L{depth} Category ID"])
+            category_name = row[f"L{depth}"]
+            class_id = int(row["Class ID"]) if "Class ID" in row else None
+            vendor_id = int(row[f"Vendor ID"])
 
-                # add node to taxonomy
-                taxonomy.add(parent_id, category_id, category_name, class_id)
+            # add node to taxonomy
+            taxonomy.add(parent_id, category_id, category_name, class_id)
 
         # parse into native data structure
-        return Taxonomy(root=self._read_from_readable_repr(readable_taxonomy))
- 
-    def write(self, dir_path: str):
+        return taxonomy
+
+
+    def to_csv(self, filepath: str):
         """Write a human readable representation of taxonomy to a file in specified dir"""
         # write to a specific file name in dir
-        with open(join(dir_path, "taxonomy.csv"), 'w') as f:
-            # get max level (e.g. if no L3 categories return 2)
-            max_level = self.get_max_level()
+        # get max level (e.g. if no L3 categories return 2)
+        max_level = self.get_max_level()
 
-            # category names, category ids, class id
-            header = [f"L{x}" for x in range(max_level)] 
-                + [f"L{x} Category ID" for x in range(max_level)] 
-                + ["Class ID"]
+        # category names, category ids, vendor ids, class id
+        header = [f"L{x}" for x in range(1, max_level + 1)] \
+            + ["Category ID", "Parent Category ID", "Vendor ID", "Class ID"]
 
-            writer = csv.DictWriter(f, fieldnames=header)
-            
-            # write each path from root to node
-            for names, ids, class_id in self._repr():
-                write_row = {}
-                for i, name in enumerate(names):
-                    write_row[f"L{i}"] = name
+        df = pd.DataFrame(columns=header)
+        
+        # write each path from root to node
+        for names, category_id, parent_category_id, vendor_id, class_id in self._repr():
+            row = {}
+            for i, name in enumerate(names):
+                row[f"L{i + 1}"] = name
 
-                for i, category_id in enumerate(ids):
-                    write_row[f"L{i} Category ID"] = category_id
+            row["Category ID"] = category_id
+            row["Parent Category ID"] = parent_category_id
+            row["Vendor ID"] = vendor_id
+            row["Class ID"] = class_id
 
-                write_row["Class ID"] = class_id
+            df = df.append(row, ignore_index=True)
 
-                writer.writerow(write_row)
+        # sort taxonomy entries by number of NaNs so ordering: L1, L2, ..., Lx categories
+        # better readability
+        df = df.iloc[df.isnull().sum(1).sort_values(ascending=False).index]
+        
+        # change cols auto inferred as floats to ints
+        integer_cols = ["Category ID", "Parent Category ID", "Vendor ID", "Class ID"]
+        df[integer_cols] = df[integer_cols].astype(pd.Int64Dtype())
+
+        # write df
+        df.to_csv(filepath, index=False)
 
     def _iter(self, node: TaxonomyNode, path: List[TaxonomyNode]):
-        """Iterate recursively and output all nodes in tree except root"""
+        """Iterate recursively and output all nodes in tree"""
+        if len(path) == 0:
+            # add root
+            path.append(node)
+
         yield node, path
 
         for child in node.children:
@@ -231,7 +275,7 @@ class Taxonomy:
             for child in node.children:
                 yield from self._iter_level(child, level - 1)
 
-    def _repr() -> List[Tuple[List[str], Tuple[List[str]], int]]:
+    def _repr(self) -> List[Tuple[List[str], int, int, int, int]]:
         """
         Return a representation of the taxonomy with one entry per node.
         Each entry is a list of names from root to node (excluding root), 
@@ -239,11 +283,11 @@ class Taxonomy:
         """
         rows = []
         for node, path in self.iter():
-            # L1, ..., Lx category names and ids
-            names = [n.category_name for n in (path[1:] + [node])]
-            ids = [n.category_id for n in (path[1:] + [node])]
+            # L1, ..., Lx category names
+            names = [n.category_name for n in path]
 
-            # add Lx category id
-            rows.append((names, ids, node.class_id))
+            # get parent category id if it exists
+            parent_id = path[-2].category_id if len(path) > 1 else None
+            rows.append((names[1:], node.category_id, parent_id, node.vendor_id, node.class_id))
 
         return rows
