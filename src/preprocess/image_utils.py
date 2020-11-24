f27adbb6abafe0f68fc17ef510dbcd3f2498477d
AbhiSivaprasad <abhi@live.com>
2020-11-24 02:27:45 +0000

    added thread pooling for download/upload

--- src/preprocess/image_utils.py
+++ src/preprocess/image_utils.py
@@ -3,8 +3,11 @@ import requests
 
 from os import listdir
 from os.path import join
+from functools import partial
 from typing import List
 from tempfile import TemporaryDirectory
+from multiprocessing.pool import ThreadPool
+
 from .utils import hash_string
 
 
@@ -21,20 +24,27 @@ def download_images_to_bucket(s3_client, bucket_name: str, object_prefix: str, u
     upload_directory_to_s3(s3_client, bucket_name, object_prefix, temp_dirpath)
 
 
-def upload_directory_to_s3(s3_client, bucket_name, object_prefix: str, dirpath: str):
+def upload_directory_to_s3(s3_client, bucket_name, object_prefix: str, dirpath: str, num_workers: int = 32):
     """Upload a directory w/o subdirectories to s3"""
-    for filename in listdir(dirpath):
-        filepath = join(dirpath, filename)
-        object_name = join(object_prefix, filename)
-        s3_client.upload_file(filepath, bucket_name, object_name)
+    filenames = [filename for filename in listdir(dirpath)]
+    filepaths = [join(dirpath, filename) for filename in filenames]
+    object_names = [join(object_prefix, filename) for filename in filenames]
+
+    # define function instead of partial since positional args needed for map
+    def thread_f(filepath, key):
+        return s3_client.upload_file(filepath, bucket_name, key)
+
+    # upload in parallel with a thread pool
+    ThreadPool(num_workers).starmap(thread_f, zip(filepaths, object_names))
 
 
-def download_images(urls: List[str], dirpath: str):
+def download_images(urls: List[str], dirpath: str, num_workers: int = 32):
     """Download images to a local dir""" 
-    for url in urls:
-        filename = f"{hash_string(url)}.jpeg"
-        filepath = join(dirpath, filename)
-        download_image(url, filepath)
+    # file name is a hash of the url
+    filepaths = [join(dirpath, f"{hash_string(url)}.jpeg") for url in urls]
+
+    # download in parallel with a thread pool
+    ThreadPool(num_workers).starmap(download_image, zip(urls, filepaths))
      
 
 def download_image(url: str, filepath: str):
@@ -57,4 +67,5 @@ if __name__ == '__main__':
         "http://cdn.doordash.com/media/photos/596e6370-be62-43f2-be7a-cdffa7ff9d70-retina-large-jpeg"
     ]
 
-    download_images_to_bucket(s3_client, "glisten-images", "train/alcohol", urls)
+    # download_images_to_bucket(s3_client, "glisten-images", "train/alcohol", urls)
+    upload_directory_to_s3(s3_client, "glisten-images", "train/baby", "tmp")
