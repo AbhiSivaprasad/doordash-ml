c163b7f56ec676d5d026f95b6f45d63ef9678afb
AbhiSivaprasad <abhi@live.com>
2020-11-24 18:24:00 +0000

    updated upload/download with parallel progress bar

--- src/preprocess/image_utils.py
+++ src/preprocess/image_utils.py
@@ -1,3 +1,4 @@
+import wandb
 import boto3
 import requests 
 
@@ -6,27 +7,14 @@ from os.path import join
 from functools import partial
 from typing import List
 from tempfile import TemporaryDirectory
-from multiprocessing.pool import ThreadPool
+import multiprocessing.pool as mpp
+from tqdm import tqdm
 
 from .utils import hash_string
 
 
-def download_images_to_bucket(s3_client, bucket_name: str, object_prefix: str, urls: List[str]):
-    """Download images from urls to s3 bucket"""
-    # create temp directory for download
-    temp_dir = TemporaryDirectory()
-    temp_dirpath = temp_dir.name
-
-    # download images from urls into dir
-    download_images(urls, temp_dirpath)
-
-    # upload directory of images
-    upload_directory_to_s3(s3_client, bucket_name, object_prefix, temp_dirpath)
-
-
-def upload_directory_to_s3(s3_client, bucket_name, object_prefix: str, dirpath: str, num_workers: int = 32):
+def upload_directory_to_s3(s3_client, bucket_name, object_prefix: str, dirpath: str, filenames: List[str], num_workers: int = 16):
     """Upload a directory w/o subdirectories to s3"""
-    filenames = [filename for filename in listdir(dirpath)]
     filepaths = [join(dirpath, filename) for filename in filenames]
     object_names = [join(object_prefix, filename) for filename in filenames]
 
@@ -35,23 +23,28 @@ def upload_directory_to_s3(s3_client, bucket_name, object_prefix: str, dirpath: 
         return s3_client.upload_file(filepath, bucket_name, key)
 
     # upload in parallel with a thread pool
-    ThreadPool(num_workers).starmap(thread_f, zip(filepaths, object_names))
+    mpp.Pool.istarmap = istarmap
+    for _ in tqdm(mpp.ThreadPool(num_workers).istarmap(thread_f, zip(filepaths, object_names)), total=len(filenames)):
+        pass
 
 
-def download_images(urls: List[str], dirpath: str, num_workers: int = 32):
+def download_images_from_urls(urls: List[str], dirpath: str, filenames: List[str], num_workers: int = 16):
     """Download images to a local dir""" 
     # file name is a hash of the url
-    filepaths = [join(dirpath, f"{hash_string(url)}.jpeg") for url in urls]
+    filepaths = [join(dirpath, filename) for url, filename in zip(urls, filenames)]
 
     # download in parallel with a thread pool
-    ThreadPool(num_workers).starmap(download_image, zip(urls, filepaths))
-     
+    mpp.Pool.istarmap = istarmap
+    for _ in tqdm(mpp.ThreadPool(num_workers).istarmap(download_image, zip(urls, filepaths)), total=len(urls)):
+        pass
+
 
 def download_image(url: str, filepath: str):
     """Download image from url to filepath"""
     response = requests.get(url)
 
     if not response.ok:
+        print(response)
         raise ValueError("Failed Request:", url)
 
     img_data = response.content 
@@ -59,13 +52,37 @@ def download_image(url: str, filepath: str):
         f.write(img_data)
 
 
+def istarmap(self, func, iterable, chunksize=1):
+    """starmap-version of imap
+    """
+    if self._state != mpp.RUN:
+        raise ValueError("Pool not running")
+
+    if chunksize < 1:
+        raise ValueError(
+            "Chunksize must be 1+, not {0:n}".format(
+                chunksize))
+
+    task_batches = mpp.Pool._get_tasks(func, iterable, chunksize)
+    result = mpp.IMapIterator(self._cache)
+    self._taskqueue.put(
+        (
+            self._guarded_task_generation(result._job,
+                                          mpp.starmapstar,
+                                          task_batches),
+            result._set_length
+        ))
+    return (item for chunk in result for item in chunk)
+
+
 if __name__ == '__main__':
     s3_client = boto3.client('s3')
 
     urls = [
+        "http://cdn.doordash.com/media/photos/eef3de6b-c3a0-42c1-9903-9673c3fbe0e6-retina-large-jpeg",
         "http://cdn.doordash.com/media/photos/1737dee7-af1f-4530-89ea-f978af01530e-retina-large-jpeg",
         "http://cdn.doordash.com/media/photos/596e6370-be62-43f2-be7a-cdffa7ff9d70-retina-large-jpeg"
     ]
 
-    # download_images_to_bucket(s3_client, "glisten-images", "train/alcohol", urls)
-    upload_directory_to_s3(s3_client, "glisten-images", "train/baby", "tmp")
+    download_images_to_bucket(s3_client, "glisten-images", "train/alcohol", urls, [str(hash(url)) for url in urls])
+    # upload_directory_to_s3(s3_client, "glisten-images", "train/baby", "tmp")
